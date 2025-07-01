bcea34ab7604292e7e166a0e6ca8ec033250ff25
Jan Hula <jan.hula@cvut.cz>
2025-07-01 09:15:53 +0200

    abstracts

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -44,14 +44,14 @@ const sampleSpeakers = [
     works: "Oxford",
     image: "../img/speakers/federico.jpg", 
     title: "Why is Long-context Reasoning with LLMs Hard?",
-    abstract: "Abstract: TBD"
+    abstract: "Abstract: Robust reasoning with LLMs over long-context is hard! Frontier models seem to hallucinate or simply output wrong answers. Trying to understand exactly what is going wrong is not an easy task, but it is important to help improve the models of tomorrow. In this talk, I will go over efforts that aim to understand more precisely why long-context reasoning is hard, what goes wrong inside current models, and ways to improve this. I will argue that a main issue that LLMs encounter is that of “over-mixing” — the fact that attention mechanisms tend to act as “averaging” operations and that this averaging destroys information as the context length increases. I will also go over positional encodings and phenomena like attention sinks and the role they play in robust reasoning. Overall, the talk aims to show how one can study failure in long-context regimes “from first principles”, by understanding the mechanisms that are used by LLMs to construct the output. "
   },
 
   { name: "Jonas Hübotter", 
     works: "ETH Zurich",
     image: "../img/speakers/jonas.jpg", 
     title: "Towards Solving Hard Problems through Test-time Training",
-    abstract: "Abstract: TBD"
+    abstract: "Abstract: Recent efforts in fine-tuning language models often rely on automatic data selection, commonly using Nearest Neighbors retrieval from large datasets. However, we theoretically show that this approach tends to select redundant data, limiting its effectiveness or even hurting performance. To address this, we introduce SIFT, a data selection algorithm designed to reduce uncertainty about the model's response given a prompt, which unifies ideas from retrieval and active learning. Whereas Nearest Neighbor retrieval typically fails in the presence of information duplication, SIFT accounts for information duplication and optimizes the overall information gain of the selected examples. We focus our evaluations on fine-tuning at test-time for prompt-specific language modeling on the Pile dataset, and show that SIFT consistently outperforms Nearest Neighbor retrieval, with minimal computational overhead. Moreover, we show that our uncertainty estimates can predict the performance gain of test-time fine-tuning, and use this to develop an adaptive algorithm that invests test-time compute proportional to realized performance gains."
   },
   { name: "Aryo Lotfi", 
     works: "EPFL",
@@ -108,7 +108,7 @@ const sampleSpeakers = [
     works: "IDEAS NCBR",
     image: "../img/speakers/gracjansq.jpg", 
     title: "What Matters in Hierarchical Search for Combinatorial Reasoning Problems?",
-    abstract: "Abstract: TBD"
+    abstract: "Abstract: Efficiently tackling combinatorial reasoning problems, particularly the notorious NP-hard tasks, remains a significant challenge for AI research. Recent efforts have sought to enhance planning by incorporating hierarchical high-level search strategies, known as subgoal methods. While promising, their performance against traditional low-level planners is inconsistent, raising questions about their application contexts. In this study, we conduct an in-depth exploration of subgoal-planning methods for combinatorial reasoning. We identify the attributes pivotal for leveraging the advantages of high-level search: hard-to-learn value functions, complex action spaces, presence of dead ends in the environment, or using data collected from diverse experts. We propose a consistent evaluation methodology to achieve meaningful comparisons between methods and reevaluate the state-of-the-art algorithms. "
   },
   { name: "Witold Drzewakowski", 
     works: "IDEAS NCBR",
